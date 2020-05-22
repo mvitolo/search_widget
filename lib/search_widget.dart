1597e3db92fc262d1a3ef954211bb729a7995b55
Matteo Vitolo <matteo.vitolo@moneyfarm.com>
2020-05-22 19:54:20 +0200

    added fix to work the tap out from the box

--- lib/search_widget.dart
+++ lib/search_widget.dart
@@ -63,6 +63,7 @@ class MySingleChoiceSearchState<T> extends State<SearchWidget<T>> {
   double listContainerHeight;
   final LayerLink _layerLink = LayerLink();
   final double textBoxHeight = 48;
+  final TextEditingController textController = TextEditingController();
 
   @override
   void initState() {
@@ -122,11 +123,13 @@ class MySingleChoiceSearchState<T> extends State<SearchWidget<T>> {
         }
       }
     });
-    KeyboardVisibility.onChange.listen((visible) {
-      if (!visible) {
-        _focusNode.unfocus();
-      }
-    });
+    KeyboardVisibilityNotification().addNewListener(
+      onChange: (visible) {
+        if (!visible) {
+          _focusNode.unfocus();
+        }
+      },
+    );
   }
 
   @override
@@ -238,54 +241,67 @@ class MySingleChoiceSearchState<T> extends State<SearchWidget<T>> {
     overlayEntry = OverlayEntry(
       builder: (context) {
         final height = MediaQuery.of(context).size.height;
-        return Positioned(
-          left: position.left,
-          width: width,
-          child: CompositedTransformFollower(
-            offset: Offset(
-              0,
-              height - position.bottom < listContainerHeight
-                  ? (textBoxHeight + 6.0)
-                  : -(listContainerHeight - 8.0),
-            ),
-            showWhenUnlinked: false,
-            link: _layerLink,
-            child: Container(
-              height: listContainerHeight,
-              margin: const EdgeInsets.symmetric(horizontal: 12),
-              child: Card(
-                color: Colors.white,
-                elevation: 5,
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(4)),
-                ),
-                child: _tempList.isNotEmpty
-                    ? Scrollbar(
-                        child: ListView.separated(
-                          padding: const EdgeInsets.symmetric(vertical: 4),
-                          separatorBuilder: (context, index) => const Divider(
-                            height: 1,
-                          ),
-                          itemBuilder: (context, index) => Material(
-                            color: Colors.transparent,
-                            child: InkWell(
-                              onTap: () => onDropDownItemTap(_tempList[index]),
-                              child: widget.popupListItemBuilder(
-                                _tempList.elementAt(index),
-                              ),
-                            ),
-                          ),
-                          itemCount: _tempList.length,
-                        ),
-                      )
-                    : widget.noItemsFoundWidget != null
-                        ? Center(
-                            child: widget.noItemsFoundWidget,
-                          )
-                        : const NoItemFound(),
+        return Stack(
+          children: <Widget>[
+            Positioned.fill(
+                child: GestureDetector(
+              onTap: dismissMenu,
+              child: Container(
+                color: Colors.transparent,
               ),
-            ),
-          ),
+            )),
+            Positioned(
+              left: position.left,
+              width: width,
+              child: CompositedTransformFollower(
+                offset: Offset(
+                  0,
+                  height - position.bottom < listContainerHeight
+                      ? (textBoxHeight + 6.0)
+                      : -(listContainerHeight - 8.0),
+                ),
+                showWhenUnlinked: false,
+                link: _layerLink,
+                child: Container(
+                  height: listContainerHeight,
+                  margin: const EdgeInsets.symmetric(horizontal: 12),
+                  child: Card(
+                    color: Colors.white,
+                    elevation: 5,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4)),
+                    ),
+                    child: _tempList.isNotEmpty
+                        ? Scrollbar(
+                            child: ListView.separated(
+                              padding: const EdgeInsets.symmetric(vertical: 4),
+                              separatorBuilder: (context, index) =>
+                                  const Divider(
+                                height: 1,
+                              ),
+                              itemBuilder: (context, index) => Material(
+                                color: Colors.transparent,
+                                child: InkWell(
+                                  onTap: () =>
+                                      onDropDownItemTap(_tempList[index]),
+                                  child: widget.popupListItemBuilder(
+                                    _tempList.elementAt(index),
+                                  ),
+                                ),
+                              ),
+                              itemCount: _tempList.length,
+                            ),
+                          )
+                        : widget.noItemsFoundWidget != null
+                            ? Center(
+                                child: widget.noItemsFoundWidget,
+                              )
+                            : const NoItemFound(),
+                  ),
+                ),
+              ),
+            )
+          ],
         );
       },
     );
@@ -299,6 +315,15 @@ class MySingleChoiceSearchState<T> extends State<SearchWidget<T>> {
     }
   }
 
+  void dismissMenu() {
+    if (overlayEntry != null) {
+      overlayEntry.remove();
+      overlayEntry = null;
+      _controller.clear();
+      _focusNode.unfocus();
+    }
+  }
+
   @override
   void dispose() {
     if (overlayEntry != null) {
@@ -308,3 +333,125 @@ class MySingleChoiceSearchState<T> extends State<SearchWidget<T>> {
     super.dispose();
   }
 }
+
+class NoItemFound extends StatelessWidget {
+  final String title;
+  final IconData icon;
+
+  const NoItemFound({
+    this.title = "No data found",
+    this.icon = Icons.folder_open,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Icon(icon, size: 24, color: Colors.grey[900].withOpacity(0.7)),
+          SizedBox(width: 10.0),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.grey[900].withOpacity(0.7),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+/// A base class to handle the subscribing events
+class KeyboardVisibilitySubscriber {
+  /// Called when a keyboard visibility event occurs
+  /// Is only called when the state changes
+  /// The [visible] parameter reflects the new visibility
+  final Function(bool visible) onChange;
+
+  /// Called when the keyboard appears
+  final Function onShow;
+
+  /// Called when the keyboard closes
+  final Function onHide;
+
+  /// Constructs a new [KeyboardVisibilitySubscriber]
+  KeyboardVisibilitySubscriber({this.onChange, this.onShow, this.onHide});
+}
+
+/// The notification class that handles all information
+class KeyboardVisibilityNotification {
+  static const EventChannel _keyboardVisibilityStream =
+      const EventChannel('flutter_keyboard_visibility');
+  static Map<int, KeyboardVisibilitySubscriber> _list =
+      Map<int, KeyboardVisibilitySubscriber>();
+  static StreamSubscription _keyboardVisibilitySubscription;
+  static int _currentIndex = 0;
+
+  /// The current state of the keyboard visibility. Can be used without subscribing
+  bool isKeyboardVisible = false;
+
+  /// Constructs a new [KeyboardVisibilityNotification]
+  KeyboardVisibilityNotification() {
+    _keyboardVisibilitySubscription ??= _keyboardVisibilityStream
+        .receiveBroadcastStream()
+        .listen(onKeyboardEvent);
+  }
+
+  /// Internal function to handle native code channel communication
+  void onKeyboardEvent(dynamic arg) {
+    isKeyboardVisible = (arg as int) == 1;
+
+    // send a message to all subscribers notifying them about the new state
+    _list.forEach((subscriber, s) {
+      try {
+        if (s.onChange != null) {
+          s.onChange(isKeyboardVisible);
+        }
+        if ((s.onShow != null) && isKeyboardVisible) {
+          s.onShow();
+        }
+        if ((s.onHide != null) && !isKeyboardVisible) {
+          s.onHide();
+        }
+      } catch (_) {}
+    });
+  }
+
+  /// Subscribe to a keyboard visibility event
+  /// [onChange] is called when a change of the visibility occurs
+  /// [onShow] is called when the keyboard appears
+  /// [onHide] is called when the keyboard disappears
+  /// Returns a subscribing id that can be used to unsubscribe
+  int addNewListener(
+      {Function(bool) onChange, Function onShow, Function onHide}) {
+    _list[_currentIndex] = KeyboardVisibilitySubscriber(
+        onChange: onChange, onShow: onShow, onHide: onHide);
+    return _currentIndex++;
+  }
+
+  /// Subscribe to a keyboard visibility event using a subscribing class [subscriber]
+  /// Returns a subscribing id that can be used to unsubscribe
+  int addNewSubscriber(KeyboardVisibilitySubscriber subscriber) {
+    _list[_currentIndex] = subscriber;
+    return _currentIndex++;
+  }
+
+  /// Unsubscribe from the keyboard visibility events
+  /// [subscribingId] has to contain an id previously returned on add
+  void removeListener(int subscribingId) {
+    _list.remove(subscribingId);
+  }
+
+  /// Internal function to clear class on dispose
+  dispose() {
+    if (_list.length == 0) {
+      _keyboardVisibilitySubscription?.cancel()?.catchError((e) {});
+      _keyboardVisibilitySubscription = null;
+    }
+  }
+}
+
